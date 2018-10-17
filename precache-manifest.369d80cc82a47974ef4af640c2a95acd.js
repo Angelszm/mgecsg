ad3e06b96892d58e821c3cbe25b9ab48531fb55b
ANGEL <swezinmyint.2912@gmail.com>
2018-10-17 16:26:26 -0700

    Updates

---
+++ precache-manifest.369d80cc82a47974ef4af640c2a95acd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mgecsg/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "52752a1846679ce950aa",
+    "url": "/mgecsg/static/js/runtime~main.52752a18.js"
+  },
+  {
+    "revision": "e6f885fd8b95f559fedd",
+    "url": "/mgecsg/static/js/main.e6f885fd.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/mgecsg/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "e6f885fd8b95f559fedd",
+    "url": "/mgecsg/static/css/main.31a1e650.chunk.css"
+  },
+  {
+    "revision": "f7d4b1afec6ff1225c8318128f7bfb31",
+    "url": "/mgecsg/index.html"
+  }
+];
